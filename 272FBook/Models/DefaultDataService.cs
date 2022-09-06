a32c6d722db2a053e6f84496d6f0c2d265f5f559
Marco Mac <mclarenmarco998@gmail.com>
2022-09-06 11:49:33 +0200

    Amper klaar, images display net nie reg nie

--- 272FBook/Models/DefaultDataService.cs
+++ 272FBook/Models/DefaultDataService.cs
@@ -11,6 +11,7 @@ namespace RandomCourseFBook.Models
     {
         private String ConnectionString;
         public static List<MarkRange> markRanges = null;
+        private SqlConnection currConnection;
 
         public DefaultDataService() {
             ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
@@ -26,11 +27,57 @@ namespace RandomCourseFBook.Models
             markRanges.Add(new MarkRange { Symbol = "A", MinOfRange = 90, MaxOfRange = 94 });
             markRanges.Add(new MarkRange { Symbol = "A+", MinOfRange = 95, MaxOfRange = 100 });
         }
+        public bool openConnection()
+        {
+            bool status = true;
+            try
+            {
+                String conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                currConnection = new SqlConnection(conString);
+                currConnection.Open();
+            }
+            catch
+            {
+                status = false;
+            }
+            return status;
+        }
 
+        public bool closeConnection()
+        {
+            if (currConnection != null)
+            {
+                currConnection.Close();
+            }
+            return true;
+
+        }
         public List<Student> getAllStudentsByMarkRange(int min, int max)
         {
             List<Student> students = new List<Student>();
             //TODO: get all students whose grade falls between min and max (inclusive)
+
+            try
+            {
+                openConnection();
+                SqlCommand command = new SqlCommand("SELECT DISTINCT Students.ID,Students.FirstName, Students.LastName,Images.B64Image from Students INNER JOIN Images ON Students.ID = Images.StudentID WHERE Grade > '" + min + "' AND Grade < '" + max + "'", currConnection);
+                using (SqlDataReader reader = command.ExecuteReader()) //lees van databasisse gebruik ExcecuteRader, NA databasis stuur ExcecuteNonQuery
+                {
+                    while (reader.Read())
+                    {
+                        Student tmpDest = new Student();
+                        tmpDest.FirstName = reader["FirstName"].ToString();
+                        tmpDest.Surname = reader["LastName"].ToString();
+                        tmpDest.ID = Convert.ToInt32(reader["ID"]);
+                        students.Add(tmpDest);
+                    }
+                }
+                closeConnection();
+            }
+            catch
+            {
+            }
+
             return students;
         }
 
@@ -39,12 +86,53 @@ namespace RandomCourseFBook.Models
         {
             List<Student> students = new List<Student>();
             //TODO: get all students whose grade falls between min and max (inclusive) and whose recorded sex matches the method's first argument
+            try
+            {
+                openConnection();
+                SqlCommand command = new SqlCommand("SELECT DISTINCT Students.ID,Students.FirstName, Students.LastName,Images.B64Image from Students INNER JOIN Images ON Students.ID = Images.StudentID WHERE Grade > '" + min + "' AND Grade < '" + max + "'", currConnection);
+                using (SqlDataReader reader = command.ExecuteReader()) //lees van databasisse gebruik ExcecuteRader, NA databasis stuur ExcecuteNonQuery
+                {
+                    while (reader.Read())
+                    {
+                        Student tmpDest = new Student();
+                        tmpDest.FirstName = reader["FirstName"].ToString();
+                        tmpDest.Surname = reader["LastName"].ToString();
+                        tmpDest.ID = Convert.ToInt32(reader["ID"]);
+                        students.Add(tmpDest);
+                    }
+                }
+                closeConnection();
+            }
+            catch
+            {
+            }
+
             return students;
         }
 
         public List<Image> getAllImages() {
             List<Image> images = new List<Image>();
             //TODO: Retrieve all the information associated with the images
+            try
+            {
+                openConnection();
+                SqlCommand command = new SqlCommand("SELECT * from Images", currConnection);
+                using (SqlDataReader reader = command.ExecuteReader()) //lees van databasisse gebruik ExcecuteRader, NA databasis stuur ExcecuteNonQuery
+                {
+                    while (reader.Read())
+                    {
+                        Image tmpDest = new Image();
+                        tmpDest.ImageRaw = "B64Image";
+                        tmpDest.StudentID = Convert.ToInt32(reader["StudentID"]);
+                        images.Add(tmpDest);
+                    }
+                }
+                closeConnection();
+            }
+            catch
+            {
+            }
+
             return images;
         }
     }
